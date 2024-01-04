3fabbc1aa4a9dec2dc538c6795baf02e04936f97
Rebecca Shoptaw <rebecca.shoptaw@gmail.com>
2024-01-04 14:37:43 -0500

    Add Enter key funtionality.

--- src/components/AskOscar.tsx
+++ src/components/AskOscar.tsx
@@ -4,6 +4,15 @@ import Input from "./Input";
 import Quote from "./Quote";
 import { PORTFOLIO_LINK } from "../data/Links";
 
+window.onload = () => {
+  document.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+      e.preventDefault();
+      document.getElementById("new-quote-btn")?.click();
+    }
+  });
+};
+
 const AskOscar = () => {
   const inputInfo = useInput();
   const { quote, handleInput, handleReset } = useNewQuote(inputInfo.input);
